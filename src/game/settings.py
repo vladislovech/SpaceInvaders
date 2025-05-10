ec12d834b26adf40162243cbbb6baaae5f70555a
Vladislove <vladislove05@mail.ru.com>
2025-05-11 02:23:00 +0500

    fix speed of game

--- src/game/settings.py
+++ src/game/settings.py
@@ -17,7 +17,7 @@ class Settings:
 
         self.fleet_drop_speed = 10
 
-        self.speedup_scale = 15
+        self.speedup_scale = 1.1
         self.score_scale = 2
 
         self.initialize_dynamic_settings()
